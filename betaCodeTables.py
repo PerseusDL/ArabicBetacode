2916103e60e7f1963636350bacefe124fee626f1
Maxim Romanov <romanov.maxim@gmail.com>
2015-02-02 15:24:25 -0500

    Additional transliteration table: Library of Congress Romanization of Arabic (translitLOC)

--- betaCodeTables.py
+++ betaCodeTables.py
@@ -1,6 +1,8 @@
 # The file includes three conversion tables.
 
-# Including some variations (*h and .h --- are the same)
+# One-to-one transliteration for computational research
+# - main principle: one Arabic character = one Latin character
+# - understandable for Arabists, but has some unconventional symbols
 betacodeTranslit = {
 # Alphabet letters
     '_a' : 'ā', # alif
@@ -55,6 +57,52 @@ betacodeTranslit = {
     'i'  : 'i', # kasrah
     }
 
+# conventional US/LOC transliteration
+translitLOC = {
+# Alphabet letters
+    'ā' : 'ā',  # alif
+    'b' : 'b',  # bā’
+    't' : 't',  # tā’
+    'ṯ' : 'th', # thā’
+    'ǧ' : 'j',  # jīm
+    'č' : 'ch', # chīm / Persian
+    'ḥ' : 'ḥ',  # ḥā’
+    'ḫ' : 'kh', # khā’
+    'd' : 'd',  # dāl
+    'ḏ' : 'dh', # dhāl
+    'r' : 'r',  # rā’
+    'z' : 'z',  # zayn
+    's' : 's',  # sīn
+    'š' : 'sh', # shīn
+    'ṣ' : 'ṣ',  # ṣād
+    'ḍ' : 'ḍ',  # ḍād
+    'ṭ' : 'ṭ',  # ṭā’
+    'ẓ' : 'ẓ',  # ẓā’
+    'ʿ' : 'ʿ',   # ‘ayn
+    'ġ' : 'gh', # ghayn
+    'f' : 'f',  # fā’
+    'ḳ' : 'q',  # qāf
+    'k' : 'k',  # kāf
+    'g' : 'g',  # gāf / Persian
+    'l' : 'l',  # lām
+    'm' : 'm',  # mīm
+    'n' : 'n',  # nūn
+    'h' : 'h',  # hā’
+    'w' : 'w',  # wāw
+    'ū' : 'u',  # wāw
+    'y' : 'y',  # yā’
+    'ī' : 'i',  # yā’
+# Non-alphabetic letters
+    'ʾ' : 'ʾ',   # hamza
+    'á' : 'ā',  # alif maqṣūrah
+    'ŧ' : 'h',  # tā’ marbūṭah
+# Vowels
+    'ã' : 'ā',  # dagger alif
+    'a' : 'a',  # fatḥah
+    'u' : 'u',  # ḍammah
+    'i' : 'i',  # kasrah
+    }
+
 # necessary for rendering searcheable lines
 translitSearch = {
 # Alphabet letters
@@ -100,47 +148,3 @@ translitSearch = {
     'u' : 'u',  # ḍammah
     'i' : 'i',  # kasrah
     }
-
-# From Wikipedia
-"""
-Arabic letters usage in Literary Arabic
-Name	Translit	Value (IPA)	Contextual forms	Isolated
-Final	Medial	Initial
-alif	ā /  ’  (also  ʾ  )	various,
-including /aː/ [a]	ـا	ـا	ا	ا
-bā’	b	/b/
-(sometimes /p/ in loanwords)[b]	ـب	ـبـ	بـ	ب
-tā’	t	/t/	ـت	ـتـ	تـ	ت
-thā’	th (also ṯ )	/θ/	ـث	ـثـ	ثـ	ث
-jīm	j (also ǧ, g)	[d͡ʒ] ~ [ʒ] ~ [ɡ] [c]	ـج	ـجـ	جـ	ج
-ḥā’	ḥ	/ħ/	ـح	ـحـ	حـ	ح
-khā’	kh (also ḫ, ḵ )	/x/	ـخ	ـخـ	خـ	خ
-dāl	d	/d/	ـد	ـد	د	د
-dhāl	dh (also ḏ )	/ð/	ـذ	ـذ	ذ	ذ
-rā’ / rāy / rays	r	/r/	ـر	ـر	ر	ر
-zayn / zāy / zā’	z	/z/	ـز	ـز	ز	ز
-sīn	s	/s/	ـس	ـسـ	سـ	س
-shīn	sh (also š )	/ʃ/	ـش	ـشـ	شـ	ش
-ṣād	ṣ	/sˤ/	ـص	ـصـ	صـ	ص
-ḍād	ḍ	/dˤ/	ـض	ـضـ	ضـ	ض
-ṭā’	ṭ	/tˤ/	ـط	ـطـ	طـ	ط
-ẓā’	ẓ	[ðˤ] ~ [zˤ]	ـظ	ـظـ	ظـ	ظ
-‘ayn	 ‘  (also  ʿ  )	/ʕ/	ـع	ـعـ	عـ	ع
-ghayn	gh (also ġ, ḡ )	/ɣ/
-(sometimes /ɡ/ in loanwords)[c]	ـغ	ـغـ	غـ	غ
-fā’	f	/f/
-(sometimes /v/ in loanwords)[b]	ـف	ـفـ	فـ	ف [d]
-qāf	q	/q/
-(sometimes /ɡ/ in loanwords)[c]	ـق	ـقـ	قـ	ق [d]
-kāf	k	/k/
-(sometimes /ɡ/ in loanwords)[c]	ـك	ـكـ	كـ	ك
-gāf	g	/g/	ـگ	ـگـ	گـ	گ
-lām	l	/l/	ـل	ـلـ	لـ	ل
-mīm	m	/m/	ـم	ـمـ	مـ	م
-nūn	n	/n/	ـن	ـنـ	نـ	ن
-hā’	h	/h/	ـه	ـهـ	هـ	ه
-wāw	w / ū / aw	/w/, /uː/, /aw/,
-sometimes /u/, /o/, and /oː/ in loanwords	ـو	ـو	و	و
-yā’	y / ī / ay	/j/, /iː/, /aj/,
-sometimes /i/, /e/, and /eː/ in loanwords	ـي	ـيـ	يـ	ي [e]
-"""
